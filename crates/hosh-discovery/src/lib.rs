df64fc46a7582d693de3e19aa870c79d11eed25b
Blake Emerson <e@emersonbenthall.com>
2026-03-02 09:06:02 -0700

    Add zec-node.cakewallet.com

--- crates/hosh-discovery/src/lib.rs
+++ crates/hosh-discovery/src/lib.rs
@@ -252,6 +252,7 @@ const ZEC_SERVERS: &[(&str, u16, bool)] = &[
     ("node.manyrz.xyz", 443, true),
     ("znodo.edgarlenon.xyz", 443, true),
     ("zec.0xrpc.io", 443, true),
+    ("zec-node.cakewallet.com", 443, true),
     ("lwd.forzcash.online", 9067, true),
     ("z3.deepikaw.xyz", 443, true),
     (
