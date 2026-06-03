6fc457846d4c62bd63c3ffbc91e27f31de52970e
Blake Emerson <e@emersonbenthall.com>
2026-06-03 14:56:43 -0500

    Added us.zec.stardust.rest:443

--- crates/hosh-discovery/src/lib.rs
+++ crates/hosh-discovery/src/lib.rs
@@ -239,6 +239,7 @@ const ZEC_SERVERS: &[(&str, u16, bool)] = &[
     ("eu.zec.stardust.rest", 443, true),
     ("eu2.zec.stardust.rest", 443, true),
     ("jp.zec.stardust.rest", 443, true),
+    ("us.zec.stardust.rest", 443, true),
     ("node.zecwhales.online", 443, true),
     ("lwd.zecbhave.uk", 443, true),
     ("zec.intertake.click", 9067, true),
